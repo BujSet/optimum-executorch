994492e497e0f8e7fea7fa7bdffc97a2701dadde
BujSet <ranganath1000@gmail.com>
2025-07-22 20:25:07 +0000

    Trying to use load model

--- test.py
+++ test.py
@@ -14,29 +14,6 @@ from transformers.testing_utils import slow
 
 from optimum.executorch import ExecuTorchModelForSpeechSeq2Seq
 from optimum.utils.import_utils import is_transformers_version
+from optimum.exporters.executorch.tasks.seq2seq_lm import load_seq2seq_lm_model
 
-model_id = "openai/whisper-tiny"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = ExecuTorchModelForSpeechSeq2Seq.from_pretrained(model_id, recipe="portable")
-processor = AutoProcessor.from_pretrained(model_id)
-
-assert(isinstance(model, ExecuTorchModelForSpeechSeq2Seq))
-assert(hasattr(model, "encoder"))
-assert(isinstance(model.encoder, ExecuTorchModule))
-assert(hasattr(model, "decoder"))
-assert(isinstance(model.decoder, ExecuTorchModule))
-dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
-sample = dataset[0]["audio"]
-
-input_features = processor(
-            sample["array"], return_tensors="pt", truncation=False, sampling_rate=sample["sampling_rate"]
-).input_features
-# Current implementation of the transcibe method accepts up to 30 seconds of audio, therefore I trim the audio here.
-input_features_trimmed = input_features[:, :, :3000].contiguous()
-
-generated_transcription = model.transcribe(tokenizer, input_features_trimmed)
-expected_text = " Mr. Quilter is the apostle of the middle classes, and we are glad to welcome his gospel. Nor is Mr. Quilter's manner less interesting than his matter. He tells us that at this festive season of the year, with Christmas and roast beef looming before us, similarly drawn from eating and its results occur most readily to the mind. He has grave doubts whether Sir Frederick Latins work is really Greek after all, and can discover that."
-logging.info(
-            f"\nExpected transcription:\n\t{expected_text}\nGenerated transcription:\n\t{generated_transcription}"
-)
-assert(generated_transcription == expected_text)
+model = load_seq2seq_lm_model("openai/whisper-tiny")
