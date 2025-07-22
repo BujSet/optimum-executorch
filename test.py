845c03fa23af4bd99282cb3dc4bffbda6ca1fd1e
BujSet <ranganath1000@gmail.com>
2025-07-22 22:17:35 +0000

    Able to lower, but can't save pte to file

--- test.py
+++ test.py
@@ -1,19 +1,36 @@
-import logging
-import os
-import subprocess
-import tempfile
-import unittest
+#import logging
+#import os
+#import subprocess
+#import tempfile
+#import unittest
 
-import pytest
-from datasets import load_dataset
+#import pytest
+#from datasets import load_dataset
 from executorch import version
-from executorch.extension.pybindings.portable_lib import ExecuTorchModule
-from packaging.version import parse
-from transformers import AutoProcessor, AutoTokenizer
-from transformers.testing_utils import slow
+from executorch.backends.arm.ethosu_partitioner import EthosUPartitioner
+from executorch.backends.arm.arm_backend import ArmCompileSpecBuilder
+from executorch.exir.backend.backend_api import to_backend
+from executorch.extension.export_util.utils import save_pte_program
+#from executorch.extension.pybindings.portable_lib import ExecuTorchModule
+#from packaging.version import parse
+#from transformers import AutoProcessor, AutoTokenizer
+#from transformers.testing_utils import slow
 
-from optimum.executorch import ExecuTorchModelForSpeechSeq2Seq
-from optimum.utils.import_utils import is_transformers_version
-from optimum.exporters.executorch.tasks.seq2seq_lm import load_seq2seq_lm_model
+#from optimum.executorch import ExecuTorchModelForSpeechSeq2Seq
+#from optimum.utils.import_utils import is_transformers_version
+from optimum.exporters.executorch.tasks.asr import load_seq2seq_speech_model
 
-model = load_seq2seq_lm_model("openai/whisper-tiny")
+speech_model = load_seq2seq_speech_model("openai/whisper-tiny")
+exports = speech_model.export()
+
+spec_builder = ArmCompileSpecBuilder().ethosu_compile_spec(
+        "ethos-u55-128",
+        extra_flags="--verbose-operators --verbose-cycle-estimate"
+        )
+compile_spec = spec_builder.build()
+encoder_ethosU = to_backend(
+    exports["encoder"],
+    EthosUPartitioner(compile_spec)
+    )
+
+save_pte_program(encoder_ethosU, "whisper_tiny_encoder_ethos_u55_128.pte")
