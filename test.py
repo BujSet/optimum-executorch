ea3f8306d166f87f0f7b4dd45274cb46542361ce
BujSet <ranganath1000@gmail.com>
2025-07-23 23:09:37 +0000

    Still will not delegate to Ethos U

--- test.py
+++ test.py
@@ -11,6 +11,12 @@ from executorch.exir import (
 )
 import torch
 from torch.export import Dim, export
+from torchao.quantization.pt2e.quantize_pt2e import convert_pt2e, prepare_pt2e
+from executorch.backends.arm.quantizer.arm_quantizer import (
+    EthosUQuantizer,
+    get_symmetric_quantization_config,
+)
+import executorch.kernels.quantized
 
 # Requires this command to be run from the executorch repo before this script can be run:
 # ./examples/arm/setup.sh --i-agree-to-the-contained-eula  --target-toolchain zephyr
@@ -20,15 +26,33 @@ exports = speech_model.export()
 
 spec_builder = ArmCompileSpecBuilder().ethosu_compile_spec(
         "ethos-u55-128",
-        extra_flags="--verbose-operators --verbose-cycle-estimate"
+        system_config="Ethos_U55_High_End_Embedded",
+        memory_mode="Shared_Sram",
+        extra_flags="--output-format=raw --debug-force-regor",
         )
+
 compile_spec = spec_builder.build()
+#torch.ops.load_library(
+#    "cmake-out-aot-lib/kernels/quantized/libquantized_ops_aot_lib.so"
+#)
 inputs = (torch.rand(1,80,3000),)
-exported_encoder = export(speech_model.encoder, inputs)
+#exported_encoder = export(speech_model.encoder, inputs)
+# Post training quantization
+graph_module = torch.export.export_for_training(speech_model.encoder, inputs).module()
+quantizer = EthosUQuantizer(compile_spec)
+operator_config = get_symmetric_quantization_config(is_per_channel=False)
+quantizer.set_global(operator_config)
+graph_module = prepare_pt2e(graph_module, quantizer)
+graph_module(*inputs)
+graph_module = convert_pt2e(graph_module)
+exported_program = torch.export.export_for_training(graph_module, inputs)
 executorch_program = to_edge_transform_and_lower(
-    exported_encoder,
-    partitioner = [EthosUPartitioner(compile_spec)]
-).to_executorch()
+    exported_program,
+    partitioner = [EthosUPartitioner(compile_spec)],
+    compile_config=EdgeCompileConfig(
+        _check_ir_validity=False,
+    )
+).to_executorch(config=ExecutorchBackendConfig(extract_delegate_segments=False))
 
 with open("whisper_tiny_encoder_ethos_u55_128.pte", "wb") as file:
     file.write(executorch_program.buffer)
