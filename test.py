7169824fcecfc39c9b84bf3d8fef76cae6bf8e01
BujSet <ranganath1000@gmail.com>
2025-07-23 21:10:30 +0000

    Delegation to ethos u seems to fail, not sure if there is a work around

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ from executorch.exir import (
     ExecutorchBackendConfig,
     to_edge_transform_and_lower,
 )
+import torch
+from torch.export import Dim, export
 
 # Requires this command to be run from the executorch repo before this script can be run:
 # ./examples/arm/setup.sh --i-agree-to-the-contained-eula  --target-toolchain zephyr
@@ -21,14 +23,25 @@ spec_builder = ArmCompileSpecBuilder().ethosu_compile_spec(
         extra_flags="--verbose-operators --verbose-cycle-estimate"
         )
 compile_spec = spec_builder.build()
-encoder_ethosU = to_backend(
-    exports["encoder"],
-    EthosUPartitioner(compile_spec)
-    )
-exec_prog = encoder_ethosU.to_executorch(
-            config=ExecutorchBackendConfig(extract_delegate_segments=False)
-)
-save_pte_program(exec_prog, "whisper_tiny_encoder_ethos_u55_128.pte")
+inputs = (torch.rand(1,80,3000),)
+exported_encoder = export(speech_model.encoder, inputs)
+executorch_program = to_edge_transform_and_lower(
+    exported_encoder,
+    partitioner = [EthosUPartitioner(compile_spec)]
+).to_executorch()
+
+with open("whisper_tiny_encoder_ethos_u55_128.pte", "wb") as file:
+    file.write(executorch_program.buffer)
+
+#encoder_ethosU = to_backend(
+#    exports["encoder"],
+#    EthosUPartitioner(compile_spec)
+#    )
+#exec_prog = encoder_ethosU.to_executorch(
+#            config=ExecutorchBackendConfig(extract_delegate_segments=False)
+#)
+#save_pte_program(exec_prog, "whisper_tiny_encoder_ethos_u55_128.pte")
+
 #edge = to_edge_transform_and_lower(
 #    exports["encoder"],
 #    partitioner=[EthosUPartitioner(compile_spec)],
